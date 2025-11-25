d0f1581ac7258c54eeb28fc933a42602f12d93dd
Liza0316 <zinkina03@gmail.com>
2025-11-25 21:14:52 +0200

    Final fixes: JWT settings, URL order, disable signals

--- practice1/urls.py
+++ practice1/urls.py
@@ -2,13 +2,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView,)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('shop.urls')),        
-    path('copy/', include('shop_copy.urls')), 
-    path('accounts/', include('accounts.urls')), 
+    path('accounts/', include('accounts.urls')),
     path('cart/', include('cart.urls')),
+    path('api/', include('api.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('copy/', include('shop_copy.urls')),
+    path('', include('shop.urls')),  
 ]
 
 if settings.DEBUG:
